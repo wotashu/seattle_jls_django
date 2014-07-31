536fbf5e921fbb67b255ca58cd06642187c33e55
Jonathan Lin <jonathanl@jcccw.org>
2014-07-31 14:55:07 -0700

    tweak html templates

--- mytest/gradebook/views.py
+++ mytest/gradebook/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 
-from gradebook.models import Enrollments, Classes
+from gradebook.models import Enrollments, Classes, Students, Teachers
 
 
 def index(request):
@@ -23,4 +23,11 @@ def list_classes(request):
     context = {'get_class_id': get_class_id}
     return render(request, 'gradebook/list_classes.html', context)
 
+
+def homeboy(request):
+    get_student_id = Students.objects.all()
+    get_teacher_id = Teachers.objects.all()
+    context_1 = {'get_student_id': get_student_id}
+    context_2 = {'get_teacher_id': get_teacher_id}
+    return render(request, 'gradebook/homeboy.html', context_1)
 # Create your views here.
