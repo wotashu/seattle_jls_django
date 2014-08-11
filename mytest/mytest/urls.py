c9d29d8a0323624fcd5a05c6d76fd5fc31a3d9a6
Jonathan Lin <jonathanl@jcccw.org>
2014-08-11 15:18:36 -0700

    temporarily remove wkhtmltopdf

--- mytest/mytest/urls.py
+++ mytest/mytest/urls.py
@@ -9,8 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^grappelli/', include('grappelli.urls')),  # grappelli URLS
-    url(r'^pdf/$', PDFTemplateView.as_view(template_name='my_template.html',
-                                           filename='my_pdf.pdf'), name='pdf'),
     url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^gradebook/', include('gradebook.urls')),
     url(r'^admin/', include(admin.site.urls)),
