540c318203ed4533bc6e4b2f0ac3cc4169cb9831
Jonathan Lin <jonathanl@jcccw.org>
2014-08-11 15:30:15 -0700

    update views to singular from models

--- mytest/gradebook/views.py
+++ mytest/gradebook/views.py
@@ -1,38 +1,38 @@
 from django.shortcuts import render
 from django.http import Http404
 
-from gradebook.models import Enrollments, Classes, Students, Teachers
+from gradebook.models import Enrollment, Class, Student, Teacher
 
 
 def index(request):
-    get_enrollment_list = Enrollments.objects.all().order_by('enrollment_id')[:500]
+    get_enrollment_list = Enrollment.objects.all().order_by('enrollment_id')[:500]
     context = {'get_enrollment_list': get_enrollment_list}
     return render(request, 'gradebook/index.html', context)
 
 
 def detail(request, enrollment_id):
     try:
-        enrollment = Enrollments.objects.get(pk=enrollment_id)
-    except Enrollments.DoesNotExist:
+        enrollment = Enrollment.objects.get(pk=enrollment_id)
+    except Enrollment.DoesNotExist:
         raise Http404
     return render(request, 'gradebook/detail.html', {'enrollment': enrollment})
 
 
 def list_classes(request):
-    get_class_id = Classes.objects.all().order_by('academic_years_academic_year_id')
+    get_class_id = Class.objects.all().order_by('academic_years_academic_year_id')
     context = {'get_class_id': get_class_id}
     return render(request, 'gradebook/list_classes.html', context)
 
 
 def homeboy(request):
-    get_student_id = Students.objects.all()
+    get_student_id = Student.objects.all()
     context = {'get_student_id': get_student_id}
     return render(request, 'gradebook/homeboy.html', context)
 
 
 def students(request, student_id):
     try:
-        student = Students.objects.get(pk=student_id)
+        student = Student.objects.get(pk=student_id)
     except Students.DoesNotExist:
         raise Http404
     context = {'student': student}
